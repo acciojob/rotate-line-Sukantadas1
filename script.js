7606361beac9b077cfe555c8b01f600c250b3889
Sukanta Das <111263085+Sukantadas1@users.noreply.github.com>
2023-12-02 08:55:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+  // Get the line element
+  const line = document.getElementById('line');
+
+  // Initial rotation angle
+  let angle = 0;
+
+  // Function to update the rotation angle every 20 milliseconds
+  function rotateLine() {
+    angle += 2;
+    line.style.transform = `rotate(${angle}deg)`;
+    requestAnimationFrame(rotateLine);
+  }
+
+  // Start the rotation
+  rotateLine();
